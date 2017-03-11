faae9a296814851e57ed6a487355aaf542982486
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-11 22:51:46 +0100

    Der Befehl 'challenge help' bietet nun eine etwas umfassendere Hilfe.

--- .bin/methods/help-challenge.js
+++ .bin/methods/help-challenge.js
@@ -1,7 +1,25 @@
 #! /usr/bin/env node
 
 function helpChallenge() {
-  console.log('help');
+  console.log(`
+    Usage
+      $ challenge <command> [options]
+
+    Command
+      new    Erstellt eine neue Aufgabe
+      start  Startet eine Aufgabe
+      run    Alias für start
+      help   Zeigt Hilfe an
+
+    Options
+      -n, --name  Der Name der Aufgabe, wenn eine erstellt wird
+      --electron  Benutzt die 'electron'-Vorlage
+      --p5        Benutzt die 'p5'-Vorlage
+
+    Examples
+      $ challenge new -n "Neue Aufgabe" --electron
+      $ challenge start "challenges/Neue Aufgabe"
+`);
 }
 
 module.exports = helpChallenge
