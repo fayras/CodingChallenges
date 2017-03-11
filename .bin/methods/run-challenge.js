1a39a976687f271d609cdca5ed6077a89223a6e5
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-11 21:44:19 +0100

    Wird beim Starten einer Aufgabe kein 'node_modules' Ordner gefunden, so wird 'npm install' ausgeführt.

--- .bin/methods/run-challenge.js
+++ .bin/methods/run-challenge.js
@@ -1,11 +1,22 @@
 #! /usr/bin/env node
 
-const spawn = require('child_process').spawn;
+const fs = require('fs');
+const { spawn, spawnSync } = require('child_process');
 const getChallengePath = require('./getChallengePath.js');
 
 function runChallenge(challenge, basePath) {
   let path = getChallengePath(challenge, basePath);
 
+  if(!fs.existsSync(`${path}/node_modules`)) {
+    console.log('node_modules not found. Installing missing dependencies...');
+    spawnSync('npm', ['install'], {
+      cwd: path,
+      shell: true,
+      stdio: 'inherit'
+    });
+  }
+
+  console.log('Starting...');
   const npm = spawn('npm', ['run', 'start'], {
     cwd: path,
     shell: true,
