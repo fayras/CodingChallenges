68875b93beb1ee25a37911dbad9d6fe9b3f2b896
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-11 23:09:00 +0100

    Beim Starten einer Aufgabe wird nun zusätzlich überprüft, ob es überhaupt Dependencies gibt und erst dann wird 'npm install' ausgeführt.

--- .bin/methods/run-challenge.js
+++ .bin/methods/run-challenge.js
@@ -5,9 +5,10 @@ const { spawn, spawnSync } = require('child_process');
 const getChallengePath = require('./getChallengePath.js');
 
 function runChallenge(challenge, basePath) {
-  let path = getChallengePath(challenge, basePath);
+  const path = getChallengePath(challenge, basePath);
+  const package = require(`${path}/package.json`);
 
-  if(!fs.existsSync(`${path}/node_modules`)) {
+  if(!fs.existsSync(`${path}/node_modules`) && package.dependencies) {
     console.log('node_modules not found. Installing missing dependencies...');
     spawnSync('npm', ['install'], {
       cwd: path,
