89248edcb42a8d164f250e69f245b24e4e5b878b
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-13 23:38:32 +0100

    Ordner 'methods' in 'commands' umbenannt.

--- .bin/challenge.js
+++ .bin/challenge.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 require('pretty-error').start().skipNodeFiles().skipPath('bootstrap_node.js');
-const CommandDispatcher = require('./methods/CommandDispatcher.js');
+const CommandDispatcher = require('./commands/CommandDispatcher.js');
 
 const userArgs = process.argv.slice(2);
 new CommandDispatcher(userArgs);
