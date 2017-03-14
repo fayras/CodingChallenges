61754fca6011943b2481ee195aac2bb168c6862d
hasanshahid <m.hasan.shahid12@gmail.com>
2017-03-14 23:04:44 +0100

    Solution for 01 Goldilocks' Bear Necessities. With comments and Es6 Systax.

--- challenges/01 Goldilocks' Bear Necessities/app.js
+++ challenges/01 Goldilocks' Bear Necessities/app.js
@@ -1,29 +1,45 @@
 var fs = require('fs');
 
-function readRows(input) {
-    var user_input_data = '';
+//function to read lines of the text file inputs.txt
+let readRows= input => {
+
+    let user_input_data = '';
 
     console.log("Available Tables for Goldlöckchen");
 
     input.on('data', function(data) {
         user_input_data += data;
-        var index = user_input_data.indexOf('\n');
 
-        user_weight=parseInt(user_input_data.substring(0, 4));
-        user_temp=parseInt(user_input_data.substring(4,7));
+        //index takes all the data and all rows
+        let index = user_input_data.indexOf('\n');
 
-        var initial=1;
-        var seat = -1;
+        //taking first row and getting the weight
+        let user_weight=parseInt(user_input_data.substring(0, 4));
+
+        //taking first row and getting the temperature
+        let user_temp=parseInt(user_input_data.substring(4,7));
+
+        //Setting the variables for the iterating of elements of text file
+        let initial=1;
+
+        let seat = -1;
+
+        //Looping through each element
         while (index > -1) {
             seat++;
-            var line = user_input_data.substring(0, index);
+
+            let line = user_input_data.substring(0, index);
+
             user_input_data = user_input_data.substring(index + 1);
+
             initial++;
-
+            //Ignoring first row of the text file
             if(initial > 2){
-                var available_weights=line.substring(0,line.indexOf('k'));
-                var temperature=line.substring(5,line.indexOf('°')).trim();
-
+                //extracting weights and considering number till 'k'
+                let available_weights=line.substring(0,line.indexOf('k'));
+                //extracting temperature and considering number till '°'
+                let temperature=line.substring(5,line.indexOf('°')).trim();
+                //Checking if both conditions meet
                 if ((temperature <= user_temp) && (available_weights >= user_weight)){
                     console.log("Seat Number : "+ seat);
                 }
@@ -31,8 +47,8 @@ function readRows(input) {
             index = user_input_data.indexOf('\n');
         }
     });
-}
-
+};
+//Reading file 'inputs.txt'
 var input = fs.createReadStream('inputs.txt');
 readRows(input);
 
