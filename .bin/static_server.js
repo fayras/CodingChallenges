5032832e8d44e887a2bd6fb95098a95d10637b55
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-27 21:46:00 +0200

    Callbacks als "arrow functions" umgeschrieben.

--- .bin/static_server.js
+++ .bin/static_server.js
@@ -6,12 +6,12 @@ const fs = require("fs");
 const PORT = 8000;
 const rootPath = process.argv[2] || process.cwd();
 
-http.createServer(function(request, response) {
+http.createServer((request, response) => {
 
   let uri = url.parse(request.url).pathname;
   let filename = path.join(rootPath, uri);
 
-  fs.exists(filename, function(exists) {
+  fs.exists(filename, exists => {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
@@ -19,9 +19,11 @@ http.createServer(function(request, response) {
       return;
     }
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+    if(fs.statSync(filename).isDirectory()) {
+      filename = path.join(filename, 'index.html');
+    }
 
-    fs.readFile(filename, "binary", function(err, file) {
+    fs.readFile(filename, "binary", (err, file) => {
       if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
@@ -36,4 +38,4 @@ http.createServer(function(request, response) {
   });
 }).listen(PORT);
 
-console.log(`Static file server running at http://localhost:${PORT}/`);
+console.info(`Static file server running at http://localhost:${PORT}/`);
