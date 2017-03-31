be42f76fff3f76544e14d865f4739fe72b2e1240
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-31 15:38:26 +0200

    Eine Info zur Eingabe der Änderungen wird vor den Fragen angezeigt.

--- .bin/commands/VersionCommand.js
+++ .bin/commands/VersionCommand.js
@@ -35,6 +35,7 @@ class VersionCommand extends Command {
     }
 
     let version = this.args._[0];
+    console.info('Bitte Änderungen eingeben. Jede Änderung mit Enter bestätigen, leere Eingabe beendet den Prompt.');
     inquirer.prompt(VersionCommand.changePrompt)
       .then(answer => this.askForChange(answer));
   }
