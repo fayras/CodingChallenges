4046be176d01d743ea765c83ba6df8a9bbedbeb6
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-31 15:34:06 +0200

    Es gibt nun ein "Version" Kommando, welches den Benutzer nach Änderungen fragt bis diese eine leere Eingabe tätigt.

---
+++ .bin/commands/VersionCommand.js
@@ -0,0 +1,43 @@
+const Command = require('./Command.js');
+const inquirer = require('inquirer');
+
+class VersionCommand extends Command {
+  constructor(args) {
+    super(args);
+    this.changes = [];
+  }
+
+  static get changePrompt() {
+    return [{
+      name: 'change',
+      message: '-'
+    }];
+  }
+
+  askForChange(answer) {
+    if (!answer.change) {
+      this.writeChangelog(this.changes);
+      return;
+    }
+
+    this.changes.push(answer.change);
+    return inquirer.prompt(VersionCommand.changePrompt)
+      .then(answer => this.askForChange(answer));;
+  }
+
+  writeChangelog(changes) {
+    console.log(changes);
+  }
+
+  run() {
+    if(this.args._.length < 1) {
+      throw new Error('Version parameter missing.');
+    }
+
+    let version = this.args._[0];
+    inquirer.prompt(VersionCommand.changePrompt)
+      .then(answer => this.askForChange(answer));
+  }
+}
+
+module.exports = VersionCommand
