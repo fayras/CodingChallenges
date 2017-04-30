0617a9cfb877d27a55df4fd4275177f57a373354
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-04-30 18:31:02 +0200

    Aufgabe 5 hinzugefügt.

---
+++ challenges/05 Mirror Mirror/MirrorEncryption.js
@@ -0,0 +1,36 @@
+/**
+ * Klassen zum Ver-/Entschlüsseln von Nachrichten
+ * mittels "Spiegel-Verschlüsselung".
+ */
+class MirrorEncryption {
+  /**
+   * Erstellt eine neue Instanz der Klasse.
+   *
+   * @param  {String} file Der Name/Pfad zur Verschlüsselungs-Datei.
+   */
+  constructor(file) {
+
+  }
+
+  /**
+   * Verschlüsselt eine Nachrichten nach der vorgegeben Verschlüsselung der Klasse.
+   *
+   * @param  {String} string Die Nachricht, welche verschlüsselt werden soll.
+   * @return {String}        Die verschlüsselte Nachricht.
+   */
+  encrypt(string) {
+
+  }
+
+  /**
+   * Entschlüsselt eine Nachricht nach der vorgegeben Verschlüsselung der Klasse.
+   *
+   * @param  {String} string Die Nachricht, welche entschlüsselt werden soll.
+   * @return {String}        Die entschlüsselte Nachricht.
+   */
+  decrypt(string) {
+
+  }
+}
+
+module.exports = MirrorEncryption;
